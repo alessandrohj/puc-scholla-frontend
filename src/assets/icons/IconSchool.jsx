cc65eb532a148337447475e7cb8d50a1a4c4a298
Alessandro de Jesus <deje0014@algonquinlive.com>
2022-09-07 22:09:20 -0400

    Created reusable icons. Added Sass and color variables

---
+++ src/assets/icons/IconSchool.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function IconSchool({ height = 70, width = 70, color = 'white' }) {
+    return (
+        <svg width={width} height={height} viewBox="0 0 70 70" fill={color} xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_d_369_278)">
+<g filter="url(#filter1_d_369_278)">
+<path d="M64.1668 26.2499L35.0002 14.5833L5.8335 26.2499L35.0002 37.9166L64.1668 26.2499ZM64.1668 26.2499V43.7499" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" shape-rendering="crispEdges"/>
+</g>
+<path d="M17.5 30.9167V46.6667C17.5 48.9874 19.3437 51.213 22.6256 52.8539C25.9075 54.4949 30.3587 55.4167 35 55.4167C39.6413 55.4167 44.0925 54.4949 47.3744 52.8539C50.6563 51.213 52.5 48.9874 52.5 46.6667V30.9167" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<defs>
+<filter id="filter0_d_369_278" x="0.333496" y="13.0833" width="69.3335" height="51.8335" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_369_278"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_369_278" result="shape"/>
+</filter>
+<filter id="filter1_d_369_278" x="0.333496" y="13.0833" width="69.3335" height="40.1667" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_369_278"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_369_278" result="shape"/>
+</filter>
+</defs>
+        </svg>
+    )
+}
