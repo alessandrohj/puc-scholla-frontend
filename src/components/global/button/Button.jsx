cb30b065ad7e96bae8c637ecb338cd0dbf3aa418
Alessandro <deje0014@algonquinlive.com>
2022-10-06 21:40:17 -0400

    Added handleClick prop

--- src/components/global/button/Button.jsx
+++ src/components/global/button/Button.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './button.scss'
 
-export default function Button({title, font, background}) {
+export default function Button({title, font = "white", background, inactive, handleCLick}) {
   return (
     <div className='button-container'>
-        <button className='basic-button' style={{backgroundColor: background, color: font}}>{title}</button>
+        {
+            inactive ? <button className='basic-button' style={{backgroundColor: 'rgba(128, 128, 128, 0.8)', color: font, opacity: 0.5, cursor: 'not-allowed'}}>{title}</button>
+            :
+            <button onClick={handleCLick} type='submit' className='basic-button' style={{backgroundColor: background, color: font, cursor: 'pointer'}}>{title}</button>
+        }
     </div>
   )
 }
