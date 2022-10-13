6b5cbd10c63e68281f8e019f52903929f8cafdd7
Alessandro <deje0014@algonquinlive.com>
2022-10-12 21:57:11 -0400

    Added getData to Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,49 @@
 import React from 'react'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { Navigate } from 'react-router-dom'
 import UserContext from '../../components/scripts/UserContext'
 
 export default function Home() {
-    const {token} = useContext(UserContext)
-    console.log('token', token)
+    const {token, setUser, user} = useContext(UserContext)
+
+    const getUserData = () => {
+        const url = "http://localhost:3000/auth/users/me";
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }})
+            .then((res) => res.json())
+            .then(({data}) => {
+               setUser(data)
+               console.log(data)
+            })
+    }
+
+    useEffect(() => {
+        getUserData()
+        }, [])
+
      return (
-    <div><h1>Home</h1> <p>{token}</p></div>
+    <div className='home'>
+        {user &&  <div>
+        <div className='home-header'>
+        <h1>Welcome {user.firstName}</h1>
+        <p>icon</p>
+        </div>
+        <div className='home-content'>
+        <div className='home-content-classes'>
+            <h2>Classes</h2>
+            <div className='home-content-classes-list'>
+            <div className='home-content-classes-list-item'>
+                <p>Class 1</p>
+                <p>icon</p>
+                </div>
+                </div>
+        </div>
+        </div>
+        </div>}
+    </div>
   ) 
 }
