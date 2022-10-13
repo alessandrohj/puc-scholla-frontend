6157af8b813718f6f0d6b8f2f06554eee7fd55f2
Alessandro <deje0014@algonquinlive.com>
2022-10-12 20:24:54 -0400

    Added api call to process login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,21 +1,60 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
 import Button from '../../components/global/button/Button'
+import Error from '../../components/global/error/Error'
 import './login.scss'
 import colors from '../../components/global/styles/variables.module.scss'
 import { Link } from 'react-router-dom'
+import UserContext from '../../components/scripts/UserContext.js'
 
 export default function Login() {
+
+  const {token, setToken} = useContext(UserContext)
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const signIn = () => {
+   if (email.length === 0 && password.length === 0) {
+      console.log('sign in')
+      return
+    } 
+    const url = "http://localhost:3000/auth/tokens";
+    // TODO: replace to deployment URL
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+    })})
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.errors) {
+          setErrorMessage(data.errors[0].message)
+        } else {
+          setToken(data.data.token)
+          setErrorMessage(null)
+          console.log(token)
+        }
+      })
+  }
+
+
   return (
     <div className='login'>
       <h1 className='login-title'>Welcome to Scholla</h1>
       <div className='login-content'>
         <div className='login-content-input'>
-          <input className='login-content-input-field' placeholder='Email' type='text'/>
-          <input className='login-content-input-field'  placeholder='Password' type='password'/>
+         {errorMessage && <Error message={errorMessage} />}
+          <input className='login-content-input-field' placeholder='Email' type='text' value={email} onChange={(ev) => setEmail(ev.target.value)}/>
+          <input className='login-content-input-field'  placeholder='Password' type='password' value={password} onChange={(ev)=> setPassword(ev.target.value)}/>
           <p className='login-content-input-highlight'>Forgot your password?</p>
         </div>
         <div className='login-content-button'>
-        <Button title={"Sign in"} font="white" background={colors.blue}/>
+        <Button title={"Sign in"} font="white" background={colors.blue} handleCLick={signIn}/>
         </div>
         <div className='login-content-footer'>
           <p>Don't have an account yet?</p>
