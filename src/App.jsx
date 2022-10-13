62802bb552b785c8c9fdd5f75aae060d86c231d3
Alessandro <deje0014@algonquinlive.com>
2022-10-12 20:23:59 -0400

    Created User Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import { Route, Routes } from 'react-router-dom'
-import './App.scss'
+import { useState } from 'react';
 import Navbar from './components/navbar/Navbar'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
+import UserContext from './components/scripts/UserContext'
+import './App.scss'
 
 function App() {
+  const [token, setToken] = useState(null)
+  const [user, setUser] = useState(null)
+  const globalData = {token, setToken, user, setUser}
 
   return (
+    <UserContext.Provider value={globalData}>
     <div className="App">
       <Routes>
         <Route exact path='/' element={<Login />} />
         <Route exact path='/signup' element={<SignUp />} />
       </Routes>
     </div>
+  </UserContext.Provider>
   )
 }
 
