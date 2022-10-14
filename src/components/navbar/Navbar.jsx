ec14d81242a86dfd884ff936779b0daf2f401890
Alessandro <deje0014@algonquinlive.com>
2022-10-13 20:17:17 -0400

    Updated navbar styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
 import { IconHome, IconSchool,IconProfile, IconTasks } from '../../assets'
 import './navbar.scss'
 
 export default function Navbar() {
 
+  const [active, setActive] = useState(false)
+
   return (
     <div className='navbar'>
         <div className='navbar-items'>
+          <Link className='navbar-items-link'>
         <IconHome />
+        </Link>
+        <Link className='navbar-items-link school'>
         <IconSchool />
+        </Link>
+        <Link  className='navbar-items-link'>
         <IconTasks />
+        </Link>
+        <Link  className='navbar-items-link'>
         <IconProfile />
+        </Link>
         </div>
     </div>
   )
