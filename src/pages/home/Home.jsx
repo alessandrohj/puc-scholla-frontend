472a5f33f7a995ee8454b805987591c79e1971ef
Alessandro de Jesus <alessandro.henri1@gmail.com>
2022-12-10 22:50:03 -0500

    Updated form styling

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,48 +1,52 @@
-import React from 'react'
-import { useContext, useEffect } from 'react'
-import UserContext from '../../components/scripts/UserContext'
-import Navbar from '../../components/navbar/Navbar'
-import Header from '../../components/global/header/Header'
-import { Navigate } from 'react-router-dom'
-import './home.scss'
+import React from "react";
+import { useContext, useEffect } from "react";
+import UserContext from "../../components/scripts/UserContext";
+import Header from "../../components/global/header/Header";
+import { Navigate } from "react-router-dom";
+import "./home.scss";
 
 export default function Home() {
-    const {setUser, user, cookies, removeCookie, setRole, role} = useContext(UserContext)
+  const { setUser, user, cookies, removeCookie, setRole } =
+    useContext(UserContext);
 
-    const getUserData = () => {
-        // const url = "https://puc-scholla-backend-production.up.railway.app/auth/users/me";
-        const url = "http://localhost:3000/auth/users/me"; //TODO: change to production url
-        fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${cookies.scholla}`
-            }})
-            .then((res) => res.json())
-            .then(({data}) => {
-               setUser(data)
-               setRole(data.role)
-               console.log(data)
-            })
-            .catch((err) => {
-                console.log(err)
-                removeCookie('scholla')
-                Navigate('/')
-            })
+  const getUserData = () => {
+    // const url = "https://puc-scholla-backend-production.up.railway.app/auth/users/me";
+    const url = "http://localhost:3000/auth/users/me"; //TODO: change to production url
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${cookies.scholla}`,
+      },
+    })
+      .then((res) => res.json())
+      .then(({ data }) => {
+        setUser(data);
+        setRole(data.role);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        removeCookie("scholla");
+        Navigate("/");
+      });
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+    if (isMounted) {
+      getUserData();
     }
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
-
-    useEffect(() => {
-       let isMounted = true
-         if (isMounted) {
-            getUserData()
-            }
-        return () => { isMounted = false }
-        }, [])
-
-     return (
-    <div className='home'>
-   {user && <Header title={`Welcome, ${user.firstName}`} section='welcome' />}
+  return (
+    <div className="home">
+      {user && (
+        <Header title={`Welcome, ${user.firstName}`} section="welcome" />
+      )}
     </div>
-  )
+  );
 }
--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -1,47 +1,77 @@
-import React, {useState, useContext} from 'react'
-import Header from '../../components/global/header/Header'
-import UserContext from '../../components/scripts/UserContext'
-import Navbar from '../../components/navbar/Navbar'
+import React, { useState, useContext } from "react";
+import Header from "../../components/global/header/Header";
+import UserContext from "../../components/scripts/UserContext";
+import "./users.scss";
 
 export default function Users() {
-    const {role} = useContext(UserContext)
-    const [schools, setSchools] = useState([])
-    if (role !== 'super') {
-        return <h1>Not Authorized</h1>
-    } else {
+  const { role } = useContext(UserContext);
+  const [schools, setSchools] = useState([]);
 
   return (
-    <div className='users'>
-        <h1>Manager Users</h1>
-    <div className='users-container'>
-    <div className='users-container-header'>
-        <h3>Add User</h3>
-        <div className='users-container-header-inputs'>
-            <input type='text' placeholder='First Name' />
-            <input type='text' placeholder='Last Name' />
-            <input type='text' placeholder='Email' />
-            <input type='text' placeholder='Password' />
-            <input type='text' placeholder='Confirm Password' />
-            <select name='role' id='role'>
-                <option value='admin'>Admin</option>
-                <option value='teacher'>Dean</option>
-                <option value='student'>Super</option>
-            </select>
-            <select name='school' id='school'>
-                {
-                    schools ? schools.map((school) => {
-                        return <option value={school.id}>{school.name}</option>
-                    }
-                    ) :
-                    <option value=''>No Schools</option>
-                }
-            </select>
+    <div className="users">
+      <Header title="Manage Users" section="newAssignment" />
+      <div className="users-container">
+        <div className="users-container-add">
+          <h3>Add User</h3>
+          <div className="users-container-add-form">
+            <div className="users-container-add-form-inputs">
+              <input
+                type="text"
+                placeholder="First Name"
+                className="users-container-add-form-inputs__first-name"
+              />
+              <input
+                type="text"
+                placeholder="Last Name"
+                className="users-container-add-form-inputs__last-name"
+              />
+              <input
+                type="text"
+                placeholder="Email"
+                className="users-container-add-form-inputs__email"
+              />
+              <input
+                type="text"
+                placeholder="Password"
+                className="users-container-add-form-inputs__password"
+              />
+              <input
+                type="text"
+                placeholder="Confirm Password"
+                className="users-container-add-form-inputs__confirm-pass"
+              />
+              <div className="users-container-add-form-inputs__role">
+                <label htmlFor="role">Role</label>
+                <select name="role" id="role">
+                  <option value="admin">Admin</option>
+                  <option value="teacher">Dean</option>
+                  <option value="student">Super</option>
+                </select>
+              </div>
+              <div className="users-container-add-form-inputs__school">
+                <label htmlFor="school">School</label>
+                <select name="school" id="school">
+                  {schools ? (
+                    schools.map((school) => {
+                      return <option value={school.id}>{school.name}</option>;
+                    })
+                  ) : (
+                    <option value="">No Schools</option>
+                  )}
+                </select>
+              </div>
+            </div>
             <button>Add User</button>
-            </div>
-            </div>
+          </div>
+        </div>
+        <div className="users-container-find">
+          <h3>Find User</h3>
+          <div className="users-container-header-inputs">
+            <input type="text" placeholder="Enter name or email" />
+            <button>Find User</button>
+          </div>
+        </div>
+      </div>
     </div>
-
-    </div>
-  )
-            }
+  );
 }
