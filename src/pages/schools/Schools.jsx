041710988f8e1ab796f499ebedac76c42e58c4db
Alessandro de Jesus <alessandro.henri1@gmail.com>
2022-12-11 21:04:28 -0500

    Added confirmation screen

--- src/pages/schools/Schools.jsx
+++ src/pages/schools/Schools.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/global/header/Header";
 import Input from "../../components/global/input/Input";
 import "./schools.scss";
 
 export default function Schools() {
+  const [addedSchool, setAddedSchool] = useState(false);
+
   function handleSearch() {
     console.log("searching");
   }
 
   function handleChange() {
     console.log("changing");
+  }
+
+  function addSchool() {
+    setAddedSchool(true);
   }
 
   return (
@@ -31,32 +37,52 @@ export default function Schools() {
               </div>
             </div>
           </div>
-          <div className="schools-add">
-            <h3>Add New School</h3>
-            <div className="schools-add-form">
-              <div className="schools-add-form-inputs">
-                <Input
-                  type="text"
-                  placeholder="School Name"
-                  id={"schoolName"}
-                  name={"schoolName"}
-                  className={"schools-add-form-inputs__school-name"}
-                  onChange={handleChange}
-                />
-                <Input
-                  type="text"
-                  id={"deanName"}
-                  name={"deanName"}
-                  placeholder="Dean"
-                  className={"schools-add-form-inputs__dean"}
-                  onChange={handleChange}
-                />
+          {!addedSchool ? (
+            <div className="schools-add">
+              <h3>Add New School</h3>
+              <div className="schools-add-form">
+                <div className="schools-add-form-inputs">
+                  <Input
+                    type="text"
+                    placeholder="School Name"
+                    id={"schoolName"}
+                    name={"schoolName"}
+                    className={"schools-add-form-inputs__school-name"}
+                    onChange={handleChange}
+                  />
+                  <Input
+                    type="text"
+                    id={"deanName"}
+                    name={"deanName"}
+                    placeholder="Dean"
+                    className={"schools-add-form-inputs__dean"}
+                    onChange={handleChange}
+                  />
+                </div>
+                <button
+                  className="schools-add-form__add-button"
+                  onClick={addSchool}
+                >
+                  Add School
+                </button>
               </div>
-              <button className="schools-add-form__add-button">
-                Add School
-              </button>
             </div>
-          </div>
+          ) : (
+            <div className="schools-add">
+              <h3>School Added</h3>
+              <div className="schools-added__content">
+                <p>School has been added successfully.</p>
+                <div className="schools-added__content-buttons">
+                  <button
+                    className="schools-added__content-buttons__confirmation"
+                    onClick={() => setAddedSchool(false)}
+                  >
+                    OK
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
